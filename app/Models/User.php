9fb85e17d2e2f0690938b7d6058fe9af40f7751c
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-09-26 16:23:50 +0300

    added chirps function to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,5 +44,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function chirps():HasMany
+    {
+
+        //defines a one-to-many relationship between this model model and the Chirp model
+        //Returs all chirps associated with this model(user)
+        return $this->hasMany(Chirp::class);
     }
 }
