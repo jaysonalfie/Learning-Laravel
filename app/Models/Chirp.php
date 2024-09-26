1ae666eb56a922dc9886fc4c7bbad10411e81588
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-09-26 16:51:13 +0300

    added db migration for the chirps table

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Chirp extends Model
 {
     use HasFactory;
+     
+    //Defines attributes that can be mass -assigned
+    protected $fillable = [
+        'message',// Only the 'message' attribute can be mass-assigned
+    ];
 }
--- database/migrations/2024_09_26_095158_create_chirps_table.php
+++ database/migrations/2024_09_26_095158_create_chirps_table.php
@@ -12,7 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('chirps', function (Blueprint $table) {
+
+            //create the primary key 'id' column
             $table->id();
+             // Create a 'user_id' column and set it as a foreign key that references the 'id' column of the 'users' table
+            // 'constrained()' automatically links to the 'users' table.
+            // 'cascadeOnDelete()' ensures that when the related user is deleted, all their chirps are also deleted
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            //Create a message column for storing the chirp content as a string
+            $table->string('message');
+              // Adds 'created_at' and 'updated_at' timestamp columns to track when the chirp is created and updated
             $table->timestamps();
         });
     }
